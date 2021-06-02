9c7a2697ecbc85d164ed35c2cf42ba20de7c8959
chenshuimei <1913405299@qq.com>
2021-06-02 21:40:44 +0800

    第三次提交

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,9 @@
+/*
+ * @Description: 这是***页面
+ * @Date: 2021-03-05 09:38:03
+ * @Author: shuimei
+ * @LastEditTime: 2021-06-02 21:40:23
+ */
 // pages/home.js
 Page({
 
@@ -12,7 +18,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //第三次提交
   },
 
   /**
